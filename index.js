e8abfdf02f9447763f42ccf5d8ab71e10adb9902
Ana Qonitah Munawwaroh <75164626+anaqonitah27@users.noreply.github.com>
2023-09-02 16:12:16 +0700

    insert data in postman success

--- index.js
+++ index.js
@@ -61,6 +61,42 @@ app.get('/kpi-utility/:id',(req,res)=>{
     })
 })
 
+// create data
+app.post('/kpi-utility', (req, res)=>{
+    // console.log('create data in kpi utility');
+    console.log(req.body, 'created_data');
+
+    let id = req.body.id;
+    let month = req.body.month;  
+    let create_date = req.body.create_date;  
+    let last_update = req.body.last_update;  
+    let user_create = req.body.user_create;
+    let Index_Electricity = req.body.Index_Electricity;
+    let Index_Steam = req.body.Index_Steam;
+    let Index_Water = req.body.Index_Water;
+    let utility_downtime = req.body.utility_downtime;
+    let mtbf = req.body.mtbf;
+    let mttr = req.body.mttr;
+    let crp = req.body.crp;
+    let Index_Low_Pres_Compresor = req.body.Index_Low_Pres_Compresor;
+    let Index_High_Pres_Compresor = req.body.Index_High_Pres_Compresor;
+
+    let queryCreated = `insert into kpi_utility(id, month, create_date, last_update, user_create, Index_Electricity, Index_Steam, Index_Water, utility_downtime, mtbf, mttr, crp, Index_Low_Pres_Compresor,Index_High_Pres_Compresor) 
+    values ('${id})','${month}','${create_date}','${last_update}','${user_create}','${Index_Electricity}','${Index_Steam}','${Index_Water}','${utility_downtime}','${mtbf}','${mttr}','${crp}','${Index_Low_Pres_Compresor}','${Index_High_Pres_Compresor}')`;
+    console.log(queryCreated, 'query-created-data');
+
+    db.query(queryCreated, (err, result) =>{
+        if(err){
+            console.log(err);
+        }
+        console.log(result, 'result');
+        res.send({
+            message: 'data inserted'
+        })
+    })
+
+})
+
 app.listen(3000, () => {
     console.log('server-running');
 })
